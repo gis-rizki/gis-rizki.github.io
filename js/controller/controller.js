f2498610030656b17558065e164eac0aabd97d19
Farhan Rizki <farhanrizki101010@gmail.com>
2023-11-16 22:31:21 +0700

    add

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -1,15 +1,6 @@
-import {
-    setInner,
-    addChild
-} from "https://jscroot.github.io/element/croot.js";
-import {
-    tableTemplate,
-    tableRowClass,
-    tableTag
-} from "../template/template.js";
-import {
-    map
-} from '../config/peta.js';
+import {setInner,addChild} from "https://jscroot.github.io/element/croot.js";
+import {tableTemplate,tableRowClass,tableTag} from "../template/template.js";
+import {map} from '../config/peta.js';
 import Draw from 'https://cdn.skypack.dev/ol/interaction/Draw.js';
 
 
@@ -37,20 +28,6 @@ export function isiRowPolyline(value) {
     }
 }
 
-// export function MakeGeojsonFromAPI(value) {
-//     // Create a GeoJSON feature collection
-//     const geojsonFeatureCollection = {
-//         type: "FeatureCollection",
-//         features: value
-//     };
-
-//     // Convert the GeoJSON feature collection to a JSON string
-//     const geojsonString = JSON.stringify(geojsonFeatureCollection, null, 2);
-
-//     // Return the JSON string
-//     return geojsonString;
-// }
-
 export function MakeGeojsonFromAPI(value) {
     const geojsonFeatureCollection = {
         type: "FeatureCollection",
@@ -67,9 +44,6 @@ export function MakeGeojsonFromAPI(value) {
 
     const link = document.createElement("a");
     link.href = url;
-    // link.download = fileName || "data.geojson"; 
-
-    // document.body.appendChild(link);
 
     return link;
 }
