07711c5d65785625a6a88a3f4c43d810dd35c3b3
Farhan Rizki <farhanrizki101010@gmail.com>
2023-11-17 01:50:29 +0700

    edit trus nih

--- js/template/template.js
+++ js/template/template.js
@@ -1,9 +1,11 @@
-import {getCookie} from "https://jscroot.github.io/cookie/croot.js";
+import {
+    getCookie
+} from "https://jscroot.github.io/cookie/croot.js";
 
 export let URLGeoJson = "https://us-central1-lofty-mark-401904.cloudfunctions.net/GISSMT5PROJECT";
 export let urlPostGCF = "https://asia-southeast2-lofty-mark-401904.cloudfunctions.net/GeoPost";
-export let UrlLogin = "https://us-central1-noted-slice-401902.cloudfunctions.net/Login-Gis"
-export let UrlRegister = "https://us-central1-noted-slice-401902.cloudfunctions.net/Register-Gis"
+export let UrlLogin = "https://asia-southeast2-lofty-mark-401904.cloudfunctions.net/logingeogis"
+export let UrlRegister = "https://asia-southeast2-lofty-mark-401904.cloudfunctions.net/registergeogis"
 export let tableTag = "tr";
 export let tableRowClass = "content is-small";
 export let token = "rizki"
