7179e710535ec5b7a16735da4b7fc019e529bbc5
Farhan Rizki <farhanrizki101010@gmail.com>
2023-11-17 00:21:12 +0700

    update file

--- js/template/template.js
+++ js/template/template.js
@@ -1,5 +1,9 @@
+import {getCookie} from "https://jscroot.github.io/cookie/croot.js";
+
 export let URLGeoJson = "https://us-central1-lofty-mark-401904.cloudfunctions.net/GISSMT5PROJECT";
 export let urlPostGCF = "https://asia-southeast2-lofty-mark-401904.cloudfunctions.net/GeoPost";
+export let UrlLogin = "https://us-central1-noted-slice-401902.cloudfunctions.net/Login-Gis"
+// export let UrlRegister = "https://us-central1-noted-slice-401902.cloudfunctions.net/Register-Gis"
 export let tableTag = "tr";
 export let tableRowClass = "content is-small";
 export let tableTemplate = `
--- jslogin/template/template.js
+++
@@ -1,4 +0,0 @@
-// url login
-export let URLLogin = `https://asia-southeast2-lofty-mark-401904.cloudfunctions.net/logingeogis`
-
-export let token = 'token';
