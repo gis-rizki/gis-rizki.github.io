42a368d7d223cd8a4e80461190695963ca970dca
Farhan Rizki <farhanrizki101010@gmail.com>
2023-11-17 11:38:56 +0700

    dd

--- js/main.js
+++ js/main.js
@@ -1,11 +1,36 @@
-import {get} from "https://jscroot.github.io/api/croot.js";
-import {getCookie} from "https://jscroot.github.io/cookie/croot.js";
-import {URLGeoJson} from "./template/template.js";
-import {MakeGeojsonFromAPI,responseData,AddLayerToMAP,drawer} from "./controller/controller.js";
-import {map} from './config/configpeta.js';
-import {onClosePopupClick,onDeleteMarkerClick,onSubmitMarkerClick,onMapClick,onMapPointerMove,disposePopover,GetLonLat} from './controller/popup.js';
-import {onClick} from 'https://jscroot.github.io/element/croot.js';
-import {getAllCoordinates} from './controller/cog.js';
+import {
+    get
+} from "https://jscroot.github.io/api/croot.js";
+import {
+    getCookie
+} from "https://jscroot.github.io/cookie/croot.js";
+import {
+    URLGeoJson
+} from "./template/template.js";
+import {
+    MakeGeojsonFromAPI,
+    responseData,
+    AddLayerToMAP,
+    drawer
+} from "./controller/controller.js";
+import {
+    map
+} from './config/configpeta.js';
+import {
+    onClosePopupClick,
+    onDeleteMarkerClick,
+    onSubmitMarkerClick,
+    onMapClick,
+    onMapPointerMove,
+    disposePopover,
+    GetLonLat
+} from './controller/popup.js';
+import {
+    onClick
+} from 'https://jscroot.github.io/element/croot.js';
+import {
+    getAllCoordinates
+} from './controller/cog.js';
 
 // let cookie = getCookie("Login")
 // if (cookie == "") {
@@ -20,7 +45,6 @@ onClick('hitungcogbutton', getAllCoordinates);
 map.on('click', onMapClick);
 map.on('pointermove', onMapPointerMove);
 map.on('movestart', disposePopover);
-
 export let allCoordinates = []
 map.on('click', function (event) {
     let lonLat = ol.proj.toLonLat(event.coordinate);
